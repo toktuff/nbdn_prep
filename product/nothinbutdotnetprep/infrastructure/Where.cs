fc319c12f3de69e39a0740df8488f4f0db67d38e
Matt Richardson <matt@redgumtech.com.au>
2010-08-30 15:31:04 +0100

    refactored to not use extension methods

--- product/nothinbutdotnetprep/infrastructure/Where.cs
+++ product/nothinbutdotnetprep/infrastructure/Where.cs
@@ -2,19 +2,28 @@ using System;
 
 namespace nothinbutdotnetprep.infrastructure
 {
-    public static class Where<ItemToFilter>
-    {
-        public static Func<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
-        {
-            return property_accessor;
-        }
-    }
 
-    public static class FuncExtensions
+    public class foo<ItemToFilter, PropertyType>
     {
-        public static Criteria<ItemToFilter> equal_to<ItemToFilter, PropertyType>(this Func<ItemToFilter, PropertyType> property_accessor, PropertyType value_to_equal)
+        private Func<ItemToFilter, PropertyType> property_accessor;
+
+        public foo(Func<ItemToFilter, PropertyType> func)
+        {
+            property_accessor = func;
+        }
+
+        public Criteria<ItemToFilter> equal_to<PropertyType>(PropertyType value_to_equal)
         {
             return new AnonymousCriteria<ItemToFilter>(item => property_accessor(item).Equals(value_to_equal));
+        }
+
+    }
+
+    public static class Where<ItemToFilter>
+    {
+        public static foo<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
+        {
+            return new foo<ItemToFilter, PropertyType>(property_accessor);
         }
     }
 }
