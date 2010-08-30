2f7a0f08f6e205ca5ed21fcf5cc9ef7ccbe4a7b9
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-08-30 15:37:10 +0100

    Adding the equaltoany method

--- product/nothinbutdotnetprep/infrastructure/Where.cs
+++ product/nothinbutdotnetprep/infrastructure/Where.cs
@@ -2,28 +2,11 @@ using System;
 
 namespace nothinbutdotnetprep.infrastructure
 {
-
-    public class foo<ItemToFilter, PropertyType>
-    {
-        private Func<ItemToFilter, PropertyType> property_accessor;
-
-        public foo(Func<ItemToFilter, PropertyType> func)
-        {
-            property_accessor = func;
-        }
-
-        public Criteria<ItemToFilter> equal_to<PropertyType>(PropertyType value_to_equal)
-        {
-            return new AnonymousCriteria<ItemToFilter>(item => property_accessor(item).Equals(value_to_equal));
-        }
-
-    }
-
     public static class Where<ItemToFilter>
     {
-        public static foo<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
+        public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
         {
-            return new foo<ItemToFilter, PropertyType>(property_accessor);
+            return new CriteriaFactory<ItemToFilter, PropertyType>(property_accessor);
         }
     }
 }
